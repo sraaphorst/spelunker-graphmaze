108f952041b3502032f58385e9b13123f8dfeda9
sraaphorst <sraaphorst@gemini.edu>
2018-07-11 12:54:55 -0400

    Initial commit.

---
+++ MazeGraph.h
@@ -0,0 +1,12 @@
+/**
+ * MazeGraph.h
+ *
+ * By Sebastian Raaphorst, 2018.
+ */
+
+#include <boost/graph/adjacency_list.hpp>
+
+namespace spelunker::mazegraph {
+
+
+}
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include "MazeGraph.h"
+
+int main() {
+    
+}
